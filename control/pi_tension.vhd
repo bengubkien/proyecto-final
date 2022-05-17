dd105ba92593ed909e1ce8220e21bab5e4538986
Benjamín <bengubkien@gmail.com>
2022-05-17 16:23:49 -0300

    Modificación en PI de tensión
    Aumentada la cantidad de bits para la representación de la fracción (de 16 bits a 26 bits).

--- control/pi_tension.vhd
+++ control/pi_tension.vhd
@@ -1,71 +1,67 @@
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.ALL;
-USE IEEE.numeric_std.ALL;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-ENTITY pi_tension IS
-  PORT( clk                               :   IN    std_logic;
-  integrator_rst:   IN    std_logic;
-  
-        ref_in                            :   IN    std_logic_vector(31 DOWNTO 0);
-        tension_in                        :   IN    std_logic_vector(31 DOWNTO 0);
-        pi_out                            :   OUT   std_logic_vector(31 DOWNTO 0)
-        );
-END pi_tension;
+entity pi_tension is
+	port (
+		clk             : in std_logic;
+		integrator_rst  : in std_logic;
+		ref_in          : in std_logic_vector(31 downto 0);
+		tension_in      : in std_logic_vector(31 downto 0);
+		pi_out          : out std_logic_vector(31 downto 0)
+	);
+end pi_tension;
+architecture rtl of pi_tension is
 
+	signal ref_in_signed : signed(31 downto 0);
+	signal tension_in_signed : signed(31 downto 0);
+	signal error_sub_temp : signed(31 downto 0);
+	signal error_out1 : signed(31 downto 0);
+	signal k_p_mul_temp : signed(63 downto 0);
+	signal k_p_out1 : signed(31 downto 0);
+	signal k_i_mul_temp : signed(63 downto 0);
+	signal k_i_out1 : signed(31 downto 0);
+	signal i_add_out1 : signed(31 downto 0);
+	signal integrator_out1 : signed(31 downto 0) := to_signed(0, 32);
+	signal clamping_out1 : signed(31 downto 0);
+	signal pi_add_out1 : signed(31 downto 0);
+	signal saturation_out1 : signed(31 downto 0);
 
-ARCHITECTURE rtl OF pi_tension IS
+begin
+	ref_in_signed <= signed(ref_in);
 
-     SIGNAL ref_in_signed                    : signed(31 DOWNTO 0);
-  SIGNAL tension_in_signed                : signed(31 DOWNTO 0);
-  SIGNAL error_out1                       : signed(31 DOWNTO 0);
-  SIGNAL k_p_mul_temp                     : signed(63 DOWNTO 0);
-  SIGNAL k_p_out1                         : signed(31 DOWNTO 0);
-  SIGNAL k_i_mul_temp                     : signed(63 DOWNTO 0);
-  SIGNAL k_i_out1                         : signed(31 DOWNTO 0);
-  SIGNAL i_add_out1                       : signed(31 DOWNTO 0);
-  SIGNAL integrator_out1                  : signed(31 DOWNTO 0) := to_signed(0, 32);
-  SIGNAL clamping_out1                    : signed(31 DOWNTO 0);
-  SIGNAL i_add_add_temp                   : signed(55 DOWNTO 0);
-  SIGNAL pi_add_out1                      : signed(31 DOWNTO 0);
-  SIGNAL saturation_out1                  : signed(31 DOWNTO 0);
+	tension_in_signed <= signed(tension_in);
 
-BEGIN
-  ref_in_signed <= signed(ref_in);
+	error_sub_temp <= ref_in_signed - tension_in_signed;
+	error_out1 <= error_sub_temp(21 downto 0) & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0';
 
-  tension_in_signed <= signed(tension_in);
+	k_p_mul_temp <= to_signed(1374389535, 32) * error_out1;
+	k_p_out1 <= resize(k_p_mul_temp(63 downto 37), 32);
 
-  error_out1 <= ref_in_signed - tension_in_signed;
+	k_i_mul_temp <= to_signed(1125899907, 32) * error_out1;
+	k_i_out1 <= resize(k_i_mul_temp(63 downto 46), 32) when integrator_rst = '0' else
+	            to_signed(0, 32);
 
-  k_p_mul_temp <= to_signed(2061584302, 32) * error_out1;
-  k_p_out1 <= resize(k_p_mul_temp(63 DOWNTO 36), 32);
+	integrator_process : process (clk)
+	begin
+		if rising_edge(clk) then
+			integrator_out1 <= i_add_out1;
+		end if;
+	end process integrator_process;
+ 
+	clamping_out1 <= to_signed(1006632960, 32) when integrator_out1 > to_signed(1006632960, 32) else
+	                 to_signed(0, 32) when integrator_out1 < to_signed(0, 32) else
+	                 integrator_out1 when integrator_rst = '0' else
+	                 to_signed(0, 32);
 
-  k_i_mul_temp <= to_signed(1576259870, 32) * error_out1;
-  k_i_out1 <= k_i_mul_temp(54 DOWNTO 23)  when integrator_rst = '0' else
-	           to_signed(0, 32);
+	i_add_out1 <= k_i_out1 + clamping_out1;
 
-  integrator_process : PROCESS (clk)
-  BEGIN
-    IF rising_edge(clk) THEN
-      integrator_out1 <= i_add_out1;
-    END IF;
-  END PROCESS integrator_process;
+	pi_add_out1 <= k_p_out1 + i_add_out1;
 
-  
-  clamping_out1 <= to_signed(983040, 32) WHEN integrator_out1 > to_signed(983040, 32) ELSE
-      to_signed(0, 32) WHEN integrator_out1 < to_signed(0, 32) ELSE
-      integrator_out1 when integrator_rst = '0' else to_signed(0, 32);
+	saturation_out1 <= to_signed(1006632960, 32) when pi_add_out1 > to_signed(1006632960, 32) else
+	                   to_signed(0, 32) when pi_add_out1 < to_signed(0, 32) else
+	                   pi_add_out1;
 
-  i_add_add_temp <= (resize(k_i_out1, 56)) + (resize(clamping_out1 & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0' & '0', 56));
-  i_add_out1 <= i_add_add_temp(54 DOWNTO 23);
+	pi_out <= std_logic_vector(saturation_out1);
 
-  pi_add_out1 <= k_p_out1 + i_add_out1;
-
-  
-  saturation_out1 <= to_signed(983040, 32) WHEN pi_add_out1 > to_signed(983040, 32) ELSE
-      to_signed(0, 32) WHEN pi_add_out1 < to_signed(0, 32) ELSE
-      pi_add_out1;
-
-  pi_out <= std_logic_vector(saturation_out1);
-
-END rtl;
-
+end rtl;
